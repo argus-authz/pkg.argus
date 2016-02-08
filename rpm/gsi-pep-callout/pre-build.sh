1ac24f020acbfdd21f48ab52fc0ff4fa57fd4fcb
Andrea Ceccanti <andrea.ceccanti@gmail.com>
2016-02-08 17:29:08 +0100

    Install globus packages at container build time

--- rpm/gsi-pep-callout/pre-build.sh
+++ rpm/gsi-pep-callout/pre-build.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 set -ex
 
-sudo yum -y install argus-pep-api-c-devel \
-  globus-gridmap-callout-error-devel \
-  globus-gssapi-gsi-devel \
-  globus-gssapi-error-devel \
-  globus-gss-assist-devel
+sudo yum -y install argus-pep-api-c-devel
